4e6f408123c9fea66c37134e77a35cdb84738086
Scottie Thompson <scottie@MacBook-Pro.local>
2020-07-27 01:50:41 -0400

    Some changes

--- stockstats/src/components/Dashboard/Dashboard.js
+++ stockstats/src/components/Dashboard/Dashboard.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DashTable from './DashTable';
+import '../../assets/css/dashboard.css';
+
+export default function Dashboard() {
+    //setting state where results will be stored
+    const [results, setResults] = useState([]);
+    //Hook equivelant to componentDidMount - pass empty array to specify run and cleanup
+    //limited to a single time
+    //This Effect gets top 10 talked about stocks for current date
+    useEffect(() => {
+        axios.get("https://stocknewsapi.com/api/v1/top-mention?&date=last7days&token=l2w6rgubmopubobdtkod8ctu2bhe6fxzhwdgsn6r").then((res) => {
+        console.log(res);
+        let api = res.data.data.all;
+        setResults(api);
+        });}, []);
+    console.log(results);
 
 
 
-class Dashboard extends React.Component {
-    render() {
-        return (
-            <div className="display">
-                <img src="https://assets.highcharts.com/images/demo-thumbnails/highcharts/line-labels-default.png" />
-                <img src="https://assets.highcharts.com/images/demo-thumbnails/highcharts/line-ajax-default.png" />
-                <div className="break"></div>
-                <img src="https://assets.highcharts.com/images/demo-thumbnails/highcharts/line-time-series-default.png" />
-            </div>
-        );
-    };
-};
 
-export default Dashboard;
+    return (
+        <section className="container">
+
+            <h1>This Week's Top Stocks By Total Mentions</h1>
+
+            <DashTable resultset = {results} />
+
+        </section>
+    );
+
+
+}
---
+++ stockstats/src/components/StockPage/StockPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+const StockPage = props => (
+    <div>
+        <h1>TSLA</h1>
+        <h2>Tesla Electric Company</h2>
+        <img src="https://www.perkinselearning.org/sites/elearning.perkinsdev1.org/files/Amazon_1.png"></img>
+        <h1>News Feed Here</h1>
+    </div>
+);
+
+export default StockPage;
